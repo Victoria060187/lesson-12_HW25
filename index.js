aea216037294aff10948bfaa3addda964d98489a
Victoria Laputko <vikalaputko87@gmail.com>
2023-08-07 22:01:49 +0300

    add new folder and files

---
+++ index.js
@@ -0,0 +1,44 @@
+class Hamburger {
+  static SIZE_SMALL = { price: 50, calories: 20 };
+  static SIZE_LARGE = { price: 100, calories: 40 };
+  static STUFFING_CHEESE = { price: 10, calories: 20 };
+  static STUFFING_SALAD = { price: 20, calories: 5 };
+  static STUFFING_POTATO = { price: 15, calories: 10 };
+  static TOPPING_MAYO = { price: 20, calories: 5 };
+  static TOPPING_SAUCE = { price: 15, calories: 0 };
+
+  constructor(size, stuffing) {
+    this.size = size;
+    this.stuffing = stuffing;
+    this.toppings = [];
+  }
+
+  addTopping(topping) {
+    this.toppings.push(topping);
+  }
+
+  calculatePrice() {
+    let totalPrice = this.size.price + this.stuffing.price;
+    for (const topping of this.toppings) {
+      totalPrice += topping.price;
+    }
+    return totalPrice;
+  }
+
+  calculateCalories() {
+    let totalCalories = this.size.calories + this.stuffing.calories;
+    for (const topping of this.toppings) {
+      totalCalories += topping.calories;
+    }
+    return totalCalories;
+  }
+}
+
+var hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+
+hamburger.addTopping(Hamburger.TOPPING_MAYO);
+console.log("Calories: " + hamburger.calculateCalories());
+console.log("Price: " + hamburger.calculatePrice());
+
+hamburger.addTopping(Hamburger.TOPPING_SAUCE);
+console.log("Price with sauce: " + hamburger.calculatePrice());
